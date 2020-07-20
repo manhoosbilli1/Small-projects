a857911c4f89d8d69ac40b0899a578cb20301cc1
Shoaib Mustafa <shoaib.mustafa7@hotmail.com>
2020-07-20 23:13:56 +0500

    finalized the project.
    Can retrieve data from firebase, create a file if it doesn't exist. input the firebase data, export that csv.... it then opens that csv to read from it to display to a custom chart table.

--- attendance-system.py
+++ attendance-system.py
@@ -13,8 +13,10 @@ day = ""
 date = ""
 salary = 0
 SrNo = 0
-k=0
 generation_started = False
+pathValid = False
+generate = False
+
 # Retrive data form Firebase
 firebase = firebase.FirebaseApplication(
     "https://citapp-65efb.firebaseio.com", None)
@@ -42,9 +44,8 @@ layout = [[sg.Text("Please type date")],
 
 
 
-          [sg.Button(button_text='Generate'), sg.Button('Display Report')],
-
-          [sg.Cancel()]]
+          [sg.Button(button_text='Generate'),
+           sg.Button('Display Report'),sg.Cancel(pad=(60,0))]]
  
 
 window = sg.Window('File Generator', layout)
@@ -92,7 +93,7 @@ def overtimeSalary(twh):
 
 ##########################################
 
-def calculations(date, mylist, tlist, namee, sal,k):
+def calculations(readPath,date, mylist, tlist, namee, sal,k):
 
     duplicates = []
     st = 'Present'
@@ -127,14 +128,14 @@ def calculations(date, mylist, tlist, namee, sal,k):
             print("       Total Woking time = ", twh)
             OT, OTS = overtimeSalary(twh)
             print(" ", sep='\n')
-    cre(k,namee, st, t1, t2, twh, OT, OTS)
+    cre(readPath,k,namee, st, t1, t2, twh, OT, OTS)
 
 
 ##########################################
-def cre(k,namee, st, t1, t2, twh, OT, OTS):
+def cre(readPath,k,namee, st, t1, t2, twh, OT, OTS):
     
 
-    filename = 'Data' + '.csv'
+    filename = readPath
 
     with open(filename, 'a+') as f:
         writer= csv.writer(f)
@@ -144,6 +145,9 @@ def cre(k,namee, st, t1, t2, twh, OT, OTS):
 
 
 ########################################
+
+
+#file create function for generate button pending...
 def Creat1():
     Sr= 'Sr.No'     
     name = 'Employee Name'
@@ -154,23 +158,23 @@ def Creat1():
     wh = 'Working Hours'
     Ot= 'Over Time'
     ots = 'OverTime Salary'
-    filename = 'Data'+'.csv'
+    filename = date+'.csv'
     #if not os.path.exists(filename):
     with open(filename,'w+') as f:
         writer= csv.writer(f)
         writer.writerow([
             Sr,name,Status,Da,Ti,to,wh,Ot,ots])
 
-
+        
         
 #########################################
 
-def generateDateReport():
+def generateDateReport(readPath):
     missing = []
     UIP = []  # Unique ID in profile_user
     GnName = []
     tlist = [None]
-
+    k=0
     timelst=[]
     duplicates=[]
     tlist = []
@@ -183,7 +187,6 @@ def generateDateReport():
 
     n = 0
     Sr= 1
-    global k
     print("date's report being generated")
     r2 = firebase.get('/user_profiles', '')
     print('Please wait.....', end="\n")
@@ -210,7 +213,7 @@ def generateDateReport():
             st = 'Missing Record'
             namee = GnName[k]
             t1, t2, twh, OT, OTS = 'None', 'None', 'None', 'None', 'None'
-            cre(Sr,namee, st, t1, t2, twh, OT, OTS)
+            cre(readPath,Sr,namee, st, t1, t2, twh, OT, OTS)
         else:
             dlist.clear()
             tlist.clear()
@@ -225,14 +228,14 @@ def generateDateReport():
             print(" ", sep='\n')
             datee = list_duplicates(dlist, date)
             if datee is not None:
-                calculations(datee, dlist, tlist, GnName[k], salary,Sr)
+                calculations(readPath,datee, dlist, tlist, GnName[k], salary,Sr)
 
             else:
                 print('absent')
                 st = 'Absent'
                 namee = GnName[k]
                 t1, t2, twh, OT, OTS = 'None', 'None', 'None', 'None', 'None'
-                cre(Sr,namee, st, t1, t2, twh, OT, OTS)
+                cre(readPath,Sr,namee, st, t1, t2, twh, OT, OTS)
 
         #n=n+1
         k = k+1
@@ -241,63 +244,73 @@ def generateDateReport():
     print('.........Report Generated.......')
     generation_started = False
     progressWindow.close()
+    sg.popup_ok('Files succesfully fetched')
+    return
         
 
 
+def show_table(date):
 
-def generateTodaysReport():
-    print("todays report being generated")
+    global pathValid
+    global generate
+    #convert date string to date
+    dataDirectory = 'D:/Downloads/test/'
+    readPath = dataDirectory + '/' + date + '.csv'
+    print('Path is:  ' + readPath)
+    if os.path.isfile(readPath) == False:
+        sg.popup_error("File does not exist. generate first.")
+        pathValid = False
 
-print(os.getcwdb())
+    if os.path.isfile(readPath) == True:
+        print("Path Already exists, now opening")
+        pathValid = True
 
-def show_table():
-    #fetch data from firebase
-    #generate the csv file 
-    
-    #####            done     #########
-    #open the csv file 
-    #seperate header and data
-    #print it
-    filename = 'H:/Data.csv'
-    if filename == '':
+    if(pathValid == True):
+        filename = readPath
+        if filename == '':
+            return
+        data = []
+        header_list = []
+        if filename is not None:
+            with open(filename, "r") as infile:
+                try:
+                    reader = csv.reader(infile)
+                    if(generate == True):
+                        header_list = ['Sr. No', 'Employee Name', 'Attendance Status', 'Date','Time In', 'Time Out', 'Working Hours','Over Time', 'OverTime Salary']
+                        
+                    else:
+                        header_list = next(reader)
+
+
+                    data = list(reader)  # read everything else into a list of rowsheader_list = ['Sr. No', 'Employee Name', 'Attendance Status', 'Date','Time In', 'Time Out', 'Working Hours','Over Time', 'OverTime Salary']
+                except:
+                    sg.popup_error("Error reading file, Path doesn't exist")
+                    return
+        sg.set_options(element_padding=(0, 0))
+
+        layout = [[sg.Table(values=data,
+                                headings=header_list,
+                                col_widths=25,
+                                size=(1000,1000),
+                                auto_size_columns=True,
+                                header_background_color='red',
+                                justification='right',
+                                
+                                # alternating_row_color='lightblue',
+                                num_rows=min(len(data), 20))]]
+
+
+        window = sg.Window('Table', layout, grab_anywhere=False,resizable=False)
+        event, values = window.read()
+        
+        
+
+        window.close()
+    else:
+        print("Path wasn't valid")
         return
-    data = []
-    header_list = []
-    if filename is not None:
-        with open(filename, "r", newline='',encoding='utf-8') as infile:
-            reader = csv.reader(infile)
-            header_list = next(reader)
-            try:
-                data = list(reader)  # read everything else into a list of rowsheader_list = ['Sr. No', 'Employee Name', 'Attendance Status', 'Date','Time In', 'Time Out', 'Working Hours','Over Time', 'OverTime Salary']
-            except:
-                sg.popup_error('Error reading file')
-                return
-    sg.set_options(element_padding=(0, 0))
 
-    layout = [[sg.Table(values=data,
-                            headings=header_list,
-                            col_widths=25,
-                            size=(1000,1000),
-                            auto_size_columns=True,
-                            header_background_color='red',
-                            justification='right',
-                            
-                            # alternating_row_color='lightblue',
-                            num_rows=min(len(data), 20))]]
-
-
-    window = sg.Window('Table', layout, grab_anywhere=False,resizable=False)
-    event, values = window.read()
-    
-    
-
-    window.close()
-
-def GenerateReport(salary, date):
-
-
-    Creat1()
-
+def GenerateReport(salary, date,readPath):
 
     n = 0
     timelst = []
@@ -316,37 +329,79 @@ def GenerateReport(salary, date):
 
 
     # value check
-    if len(day) > 0:
-        isToday = True
-        print("is today true")
     if len(salary)> 0:
         isValid = True
         print("is Valid true")
-    else:
-        sg.Popup("Please fill the salary field")
-        return
     if len(date) > 0:
         print("is date true")
         isDate = True
-
-    if isToday == True and isDate == True:
-        sg.Popup("Please choose either date or day")
-        return
-
-    if(isToday == True and isValid == True):
-        sg.PopupAutoClose("Generating todays report")
-        generateTodaysReport()
-    elif(isDate == True and isValid == True):
-        generateDateReport()
+    if(isDate == True and isValid == True):
+        generateDateReport(readPath)
 
     
     
+def GenerateReportExplicit(salary, date):
+
+    n = 0
+    timelst = []
+    duplicates = []
+    tlist = []
+    dlist = []
+    Pro_names = []
+    Emp_names = []
+    absent = []
+    FMT = '%H:%M:%S'
+    ymd = '%Y-%m-%d'
+    isValid = False
+    isDate = False
+    isToday = False
+    #Creat1()
 
 
+
+        
+
+
+    # value check
+    if len(salary)> 0:
+        isValid = True
+        print("is Valid true")
+    if len(date) > 0:
+        print("is date true")
+        isDate = True
+    path = date + '.csv'
+    if(isDate == True and isValid == True):
+        dataDirectory = 'D:/Downloads/test/'
+        readPath = os.path.join(dataDirectory + date + '.csv')
+        createPath = os.path.join(date, '.csv')
+        print('Path is:  ' + readPath)
+        if os.path.isfile(readPath) == False:
+            try:
+                with open(createPath,'w+', '', 'utf-8') as f:
+                    writer= csv.writer(f)
+
+                #header supplied manually
+            except:
+                print("Couldn't create file")
+
+            try:
+                print("Fetching data")
+                GenerateReport(salary, date,readPath)
+                pathValid = True
+            except:
+                print("File generation Failed, check path")
+                sg.popup_ok("Error Opening file, Path doesn't exist")
+                return
+
+        else:
+            print("Path exists, now opening")
+            pathValid = True
+            print("Path exists now generating data")
+            generateDateReport(path)
+            return
 
 while True:
     event, values = window.read()
-    
     
     if event == sg.WINDOW_CLOSED or event == 'Cancel':
         print("program exitting")
@@ -354,19 +409,36 @@ while True:
     if event == 'Generate':
         salary = str(values['salary'])
         date = str(values['date'])
-        print('you typed salary',salary, 'you type date', date)
-        GenerateReport(salary, date)
+        if date == '' or salary == '':
+            sg.popup_ok('Please fill the fields correctly')
+        else:
+            generate = False
+            GenerateReportExplicit(salary, date)
+        
 
         
     if event == 'Display Report':
         #the excel data display code goes in here
-        show_table()
-        print("Printing excel data")
-    if generation_started == True:
-        print("generation started")
-
-
+        salary = str(values['salary'])
+        date = str(values['date'])
+        if date == '':
+            sg.popup_ok("Please fill date and salary")
+        else:
+            generate = True   #we 
+            show_table(date)
+    
 
 
 
 window.close()
+
+
+#todo 
+
+# 1- make a single button to display as well as generate the data based on the data providied
+# 2- create a popup after every generation asking wether the user wants to display the results or not. will be autoclose
+# 3- shrinkify the code by creating seperate functions and calling them from root instead of function inside of a function 
+# 4- make the file creation and reading more robust. 
+# 5- make the reading from firebase quick 
+# 6- make an option so that the user can choose any csv from his computer instead of firebase if he wishes
+# 7- abillity to open multiple windows for every file using  OOP
